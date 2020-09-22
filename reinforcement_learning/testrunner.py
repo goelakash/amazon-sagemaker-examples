54b53b2eb0eea0301402a7dbf3252b3c12c4d70f
Akash Goel <goelakas@amazon.com>
2020-09-22 14:41:04 -0700

    Testing output for test-automation

--- reinforcement_learning/testrunner.py
+++ reinforcement_learning/testrunner.py
@@ -4,13 +4,15 @@
 # In[ ]:
 
 
-import papermill
 import itertools
-import yaml
-import os
-import sys
-import time
 import json
+import os
+import pandas as pd
+import papermill
+import sys
+from tabulate import tabulate
+import time
+import yaml
 
 
 # In[ ]:
@@ -18,8 +20,11 @@ import json
 
 # CONSTANTS
 NOTEBOOKS_LIST_CONFIG_FILE = 'testnotebooks.yml'
-global EXCEPTION_COUNT
-EXCEPTION_COUNT = 0
+SUCCESSES = 0
+EXCEPTIONS = 0
+SUCCESSFUL_EXECUTIONS = []
+FAILED_EXECUTIONS = []
+CELL_EXECUTION_TIMEOUT_SECONDS = 600
 
 
 # In[ ]:
@@ -47,14 +52,33 @@ def run_notebook(nb_path, config_file_name):
     output_nb_name = "output_{}.ipynb".format(time.time())
     os.chdir(dir_name)
     nb_test_config = load_yaml(config_file_name)
+    global SUCCESSES
+    global EXCEPTIONS
     for params in get_param_combinations(nb_test_config):        
         print("Executing: " + nb_name + " with parameters " + str(params))
         try:
-            papermill.execute_notebook(nb_name, output_nb_name, parameters=params)
+            papermill.execute_notebook(nb_name, output_nb_name, parameters=params, execution_timeout=CELL_EXECUTION_TIMEOUT_SECONDS)
+            SUCCESSES += 1
+            SUCCESSFUL_EXECUTIONS.append(dict({'notebook':nb_name, 'params':params}))
         except BaseException as error:
             print('An exception occurred: {}'.format(error))
-            EXCEPTION_COUNT += 1
-            
+            EXCEPTIONS += 1
+            FAILED_EXECUTIONS.append(dict({'notebook':nb_name, 'params':params}))
+
+def print_notebook_executions(nb_list_with_params):
+    # This expects a list of dict type items.
+    # E.g. [{'nb_name':'foo', 'params':'bar'}]
+    if not nb_list_with_params:
+        print("None")
+        return
+    vals = []
+    for nb_dict in nb_list_with_params:
+        val = []
+        for k,v in nb_dict.items():
+            val.append(v)
+        vals.append(val)
+    keys = [k for k in nb_list_with_params[0].keys()]
+    print(tabulate(pd.DataFrame([v for v in vals], columns=keys), showindex=False))
 
 
 # In[ ]:
@@ -81,6 +105,16 @@ for nb in test_notebooks_list:
     config_file_name = main_config['configs'][nb]
     print("Config file: {}".format(config_file_name))
     run_notebook(nb, config_file_name)
+
+
+# In[ ]:
+
+
+print("Summary: {}/{} tests passed.".format(SUCCESSES, SUCCESSES + EXCEPTIONS))    
+print("Successful executions: ")
+print_notebook_executions(SUCCESSFUL_EXECUTIONS)
+print("Failed executions: ")
+print_notebook_executions(FAILED_EXECUTIONS)
 
 
 # In[ ]:
